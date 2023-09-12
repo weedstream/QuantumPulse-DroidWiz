049b4c4c3e91070180f7727cee994bb4e4809009
weedstream <dede.ipang426@gmail.com>
2023-09-13 04:37:58 +0700

    create kotlin project

---
+++ Memulai Pemrograman Kotlin/kotlin-fundamental/src/main/kotlin/Main.kt
@@ -0,0 +1,157 @@
+// Kotlin Fundamental
+fun main () {
+//    val name = "Fauzi"
+//
+//    print("Hello my name is ")
+//    println(name)
+//    print(if (true) "Always true" else "Always else")
+
+//    println(" ")
+
+// Data Types & Variable
+//    var company: String = "Dicoding"
+//    company = "Dicoding Academy"
+//
+//    print(company)
+//
+//    println(" ")
+//
+//    val firstWord = "Dicoding"
+//    val lastWord = "Academy"
+//    print(firstWord + lastWord)
+//
+//    println(" ")
+//
+//    val valueA: Int = 10
+//    val valueB = 20
+//    print(valueA + valueB)
+//
+//    println(" ")
+
+// Char
+//    var vocal = 'A'
+//
+//    println("Vocal " + vocal++)
+//    println("Vocal " + vocal++)
+//    println("Vocal " + vocal++)
+//    println("Vocal " + vocal--)
+//    println("Vocal " + vocal--)
+//    println("Vocal " + vocal--)
+//    println("Vocal " + vocal--)
+//
+//    println(" ")
+
+// String
+    // val text  = "Kotlin"
+    // val firstChar = text[0]
+
+    // println("First character of $text is $firstChar")
+
+    // for (char in text){
+    //     print(("$char "))
+    // }
+
+    // println(" ")
+
+    // val statement = "Kotlin is \"Awesome!\""
+
+    // println(statement)
+    /*
+    \t: menambah tab ke dalam teks.
+
+    \n: membuat baris baru di dalam teks.
+
+    \’: menambah karakter single quote kedalam teks.
+
+    \”: menambah karakter double quote kedalam teks.
+
+    \\: menambah karakter backslash kedalam teks.
+    */
+    // println(" ")
+
+// Unicode
+    // val name = "Unicode test: \u00A9"
+    // println(name)
+    
+    // println(" ")
+
+// Raw String
+    // val line = "Line 1\n" +
+    //         "Line 2\n" +
+    //         "Line 3\n" +
+    //         "Line 4\n" +
+    //         "Line 5\n"
+    // val line2 = """
+    //     Line 1
+    //     Line 2
+    //     Line 3
+    //     Line 4
+    //     Line 5
+    // """.trimIndent()
+    // print(line2)
+
+    // println(" ")
+
+// Function
+    // val user = setUser("Fauzi", 20)
+    // println(user)
+    // printUser("Fauzi")
+
+    // print(" ")
+
+// If expression
+    // val openHours = 7
+    // val now = 7
+    // val office: String
+    // if (now > openHours) {
+    //     office = "Office already open"
+    // } else {
+    //     office = "Office is closed"
+    // }
+    // print(office)
+
+    // office = if (now > openHours) {
+    //     "Office already open"
+    // } else {
+    //     "Office is closed"
+    // }
+    // print(office)
+
+    // office= if (now > 7) {
+    //     "Office already open"
+    // }
+    // else if (now == openHours) {
+    //     "Wait a minute, office will be open"
+    // }
+    // else {
+    //     "Office is closed"
+    // }
+    // print(office)
+
+    // println(" ")
+
+// Boolean
+// Conjuction atau AND (&&)
+    // val officeOpen = 7
+    // val oficeClosed = 16
+    // val noww = 20
+
+    // val isOpen = if (noww >= officeOpen && noww <= oficeClosed) {
+    //     true
+    // } else {
+    //     false
+    // } 
+    // print("Office is open : $isOpen")
+
+    // val isOpen = noww >= officeOpen && noww <= oficeClosed
+    // print("Office is open : $isOpen")
+
+// Disjunction atau OR (||)
+    
+
+}
+// fun setUser(name: String, age: Int) = "Your name is $name, and you $age years old"
+
+// fun printUser(name: String) {
+//     println("Your name is $name")
+// }
